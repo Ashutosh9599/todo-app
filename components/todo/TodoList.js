4f8b60f1f5dc697d8d1c8125472379bf72fad6b6
Ashutosh9599 <ashutoshdas091@gmail.com>
2024-03-05 18:05:37 +0530

    Complete

--- components/todo/TodoList.js
+++ components/todo/TodoList.js
@@ -4,11 +4,13 @@ import TodoItem from './TodoItem';
 function TodoList({ todos }) {
     if (!todos || todos.length <= 0) {
         return (
+
             <section>
                 <p style={{ marginLeft: '1.75em' }}>
                     No tasks yet. Click on the input to add a new task
                 </p>
             </section>
+            
         );
     }
     return (
